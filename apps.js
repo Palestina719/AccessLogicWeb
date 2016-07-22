1d42ea567d7f82c456ca133b48f231a48f4a7ec1
Palestina719 <m_palestina719@hotmail.com>
2016-07-22 13:01:26 -0500

    agregue vista de usuario

--- apps.js
+++ apps.js
@@ -6,6 +6,6 @@ app.set("view engine","jade");
 app.use(express.static("public"));
 
 app.get("/",function(solicitud,respuesta){
-	respuesta.render("calendario");
+	respuesta.render("usuario");
 });
 app.listen(8080);
