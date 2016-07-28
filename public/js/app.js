af198b1b0b349c63500e2a1529f14f8b00870865
Palestina719 <m_palestina719@hotmail.com>
2016-07-27 21:55:01 -0500

    actualizacion de vista calendario

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,19 @@
 $(window).bind("load", function () {
     var footer = $("#footer");
+    var dash=$("dash");
     var pos = footer.position();
+    var pos1 =dash.position();
     var height = $(window).height();
+
     height = height - pos.top;
     height = height - footer.height();
     if (height > 0) {
         footer.css({
             'margin-top': height + 'px'
+
+        });
+        dash.css({
+            'height': height + 'px'
         });
     }
 });
